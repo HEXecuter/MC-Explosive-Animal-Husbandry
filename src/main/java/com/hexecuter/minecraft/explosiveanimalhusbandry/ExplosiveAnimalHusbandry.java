45b37ce8616beb0a0ee160c6951fee2d18853d25
BuildTools <ealvarez2025@gmail.com>
2023-01-03 23:00:02 -0700

    Create maven file and project boiler plate

---
+++ src/main/java/com/hexecuter/minecraft/explosiveanimalhusbandry/ExplosiveAnimalHusbandry.java
@@ -0,0 +1,17 @@
+package com.hexecuter.minecraft.explosiveanimalhusbandry;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ExplosiveAnimalHusbandry extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
