cac6c2767dc9efd490fb5b499e770139dabf052f
BuildTools <ealvarez2025@gmail.com>
2023-01-31 01:26:53 -0700

    Tweak item velocity and delay event handling Tweaked the velocity of each item spawned to not fly too far away. Actions performed by the event handler get scheduled to run 3 seconds after the event dispatches using runTaskLater.

--- src/main/java/com/hexecuter/minecraft/explosiveanimalhusbandry/helpers/HelperFunctions.java
+++ src/main/java/com/hexecuter/minecraft/explosiveanimalhusbandry/helpers/HelperFunctions.java
@@ -5,12 +5,13 @@ import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.entity.Animals;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.ExperienceOrb;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.loot.LootContext;
 import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Random;
 
 public class HelperFunctions {
@@ -25,24 +26,27 @@ public class HelperFunctions {
 
     public static void createExplosions(Location location, int count) {
         World world = location.getWorld();
-        world.spawnParticle(Particle.EXPLOSION_NORMAL, location, count, 2, 2, 2);
+        world.spawnParticle(Particle.EXPLOSION_HUGE, location, count, 1, 0, 1);
         world.playSound(location, Sound.ENTITY_GENERIC_EXPLODE, 1.0f, 1.0f);
     }
 
     public static void spawnItems(Location location, ArrayList<ItemStack> itemsToSpawn) {
         World world = location.getWorld();
         Random random = new Random();
-        int numOfDrops = random.nextInt(5) + 5;
+        int numOfDrops = random.nextInt(15) + 5;
 
 
         for (int i = 0; i < numOfDrops; i++) {
             ItemStack itemToDrop = getRandomItem(itemsToSpawn);
-            itemToDrop.setAmount(random.nextInt(64) + 1);
-            double xVelocity = random.nextDouble(3) * (random.nextBoolean() ? -1 : 1);
-            double yVelocity = random.nextDouble(3) * (random.nextBoolean() ? -1 : 1);
-            double zVelocity = 5;
+            itemToDrop.setAmount(random.nextInt(16) + 1);
+            double xVelocity = random.nextDouble(.3) * (random.nextBoolean() ? -1 : 1);
+            double zVelocity = random.nextDouble(.3) * (random.nextBoolean() ? -1 : 1);
+            double yVelocity = 1;
 
             world.dropItem(location, itemToDrop).setVelocity(new Vector(xVelocity, yVelocity, zVelocity));
+            ExperienceOrb xpOrb = (ExperienceOrb) world.spawnEntity(location, EntityType.EXPERIENCE_ORB);
+            xpOrb.setExperience(10);
+            xpOrb.setVelocity(new Vector(xVelocity, yVelocity, zVelocity));
         }
 
     }
--- src/main/java/com/hexecuter/minecraft/explosiveanimalhusbandry/listeners/EntityBreedEventListener.java
+++ src/main/java/com/hexecuter/minecraft/explosiveanimalhusbandry/listeners/EntityBreedEventListener.java
@@ -1,8 +1,12 @@
 package com.hexecuter.minecraft.explosiveanimalhusbandry.listeners;
 
+import com.hexecuter.minecraft.explosiveanimalhusbandry.ExplosiveAnimalHusbandry;
 import com.hexecuter.minecraft.explosiveanimalhusbandry.helpers.HelperFunctions;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Animals;
+import org.bukkit.entity.Breedable;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -16,13 +20,23 @@ public class EntityBreedEventListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onEntityBreedEvent(EntityBreedEvent event) {
         LivingEntity baby = event.getEntity();
+        LivingEntity player = event.getBreeder();
+        LivingEntity father = event.getFather();
+        LivingEntity mother = event.getMother();
         boolean isAnimal = baby instanceof Animals;
+        boolean isPlayer = player instanceof Player;
 
-        if (!isAnimal) return;
+        if (!isAnimal || !isPlayer) return;
 
-        ArrayList<ItemStack> lootFromEntity = HelperFunctions.getLootList((Animals) baby);
-        HelperFunctions.createExplosions(baby.getLocation(), 5);
+        Bukkit.getScheduler().runTaskLater(ExplosiveAnimalHusbandry.instance, () -> {
+            ArrayList<ItemStack> lootFromEntity = HelperFunctions.getLootList((Animals) baby);
+            HelperFunctions.createExplosions(baby.getLocation(), 3);
+            HelperFunctions.spawnItems(baby.getLocation(), lootFromEntity);
+            baby.damage(100);
 
-
+            player.sendMessage("No holding hands before marriage!");
+            ((Breedable) father).setBreed(true);
+            ((Breedable) mother).setBreed(true);
+        }, 60L);
     }
 }
